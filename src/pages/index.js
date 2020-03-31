a2d6cf3303049ffe8d9824ec7b4a1879f34ea3f1
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-03-30 21:16:40 -0700

    Working on some css and layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
-
-export default () => <>
+export default () => (
+  <Layout>
     <h1>Home</h1>
     <p>Hello San Francisco!</p>
-    <Link to='/about/'>Learn About me! &rarr;</Link>
-</>;
+    <Link to="/about/">Learn About me! &rarr;</Link>
+  </Layout>
+);
