941522355901a41b4f2802615ab384cd1f99ad8b
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-03-31 17:21:58 -0700

    Going over markdown use in a website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    plugins:['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'FrontEnd Masters Gatsby Workshop',
+    description:
+      'A Site we build together in the front-end masters workshop course!',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
